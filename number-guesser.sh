2b5b8ecc5fa30c82123d1e512006094e26a78724
FromWau <tobias.frommhund@aon.at>
2023-07-02 23:23:37 +0200

    made game good

--- number-guesser.sh
+++ number-guesser.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
+numbers=("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten")
+index=$(( RANDOM % ${#numbers[@]} ))
+NUMBER="${numbers[$index]}"
+
 VOSK_MODEL_PATH="$HOME/.local/share/vosk-models/vosk-model-en-us-0.22/"
-NUMBER=$(( RANDOM % 10 + 1 ))
 
 talk() {
-    notify-send "AI" "thinking..."
-    notify-send "AI" "User input: $1"
-    bark "$1"
+    notify-send "AI" "$1"
+    bark "$1" >/dev/null
 }
 
 listen() {
-    notify-send "AI" "I am listening for 3s..."
+    talk "I am listening for 3 seconds."
+    notify-send "AI" "listening..."
     timeout 3s pw-record --target 121 ~/.cache/audio.wav # WIP get record source
-    notify-send "AI" "stop talking"
+    talk "Stop talking. I am thinking... "
 
-    echo "$(vosk-transcriber -m $VOSK_MODEL_PATH -i ~/.cache/audio.wav)"
+    vosk-transcriber -m "$VOSK_MODEL_PATH" -i ~/.cache/audio.wav 2>/dev/null
 }
 
+isFirstRound=true
+
 while true; do
-    talk "Please guess a number between 1 and 10."
+    if [ "$isFirstRound" = true ]; then
+        talk "Please guess a number between 1 and 10."
+    else
+        talk "[sighs] Incorrect number, you guessed: $user_number. ... Please try again to guess the correct number between 1 and 10."
+    fi
 
-    user_number=$(listen)
+    user_number="$(listen)"
 
-    if [[ "$user_number" -eq "$NUMBER" ]]; then
-        echo "Congratulations! $user_number is the correct number."
+    if [[ "$user_number" == "$NUMBER" ]]; then
+        talk "[laughs] ♪ Congratulations! $user_number is the correct number."
         break
     else
-        echo "Incorrect number you guessed $user_number. Try again."
+        isFirstRound=false
     fi
 done
 
